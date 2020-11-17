450247a6f3ae32167dfeeaead34d6e6532fd3bb6
Nicholas Lucasti <nicholaslucasti@Nicholass-MBP-2.attlocal.net>
2020-11-16 19:51:57 -0500

    Adding RegEx for phone validation

--- clean_phone.py
+++ clean_phone.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
 def validate_phone(phone_number):
-    return True
+    """
+    Tests if phone numbers are valid
 
+    Arguments:
+    phone_number - A Pandas Series of phone_numbers as string object
 
-print(validate_phone(True))
+    Return:
+    A boolean Pandas Series
+    """
+    bool_phone = phone_number.str.contains("^\d{3}[-]?\d{3}[-]?\d{3}")
+    return bool_phone
+
+numbers = pd.Series(['531-231-1231','235-123-2352','23-12-41'])
+print(validate_phone(numbers))
